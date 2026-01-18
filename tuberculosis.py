7108cc5b7aab65c97b51e3a436ec941ecf0690fe
Liana Darwisyah binti Azman <152801973+lianadarwisyah@users.noreply.github.com>
2026-01-18 18:56:03 +0800

    Update tuberculosis.py

--- tuberculosis.py
+++ tuberculosis.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
